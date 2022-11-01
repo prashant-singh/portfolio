578f52eb793530d6db9fea5ed50fa2abf4b1bd8b
prashant-singh <prashant-singh@users.noreply.github.com>
2022-11-01 11:11:09 +0000

    deploy: c27b59da84491d80642461be4e1472c2233af819

--- _nuxt/static/1667300708/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1667300708",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"03f9f9ed"}}}}(null));
